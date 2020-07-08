c53936e3b5d53ae4bb4d7a64bd8f612865edaade
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-07-08 12:36:27 -0700

    adds Day 3 lecture notes

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -75,6 +75,8 @@ class HashTable:
 
         for byte in string_bytes:
             total += byte
+            # NEW (clamp to 32 bits)
+            total &= 0xffffffff
         
         return total
 
@@ -179,3 +181,155 @@ if __name__ == "__main__":
     # Test if data intact after resizing
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
+
+    
+    # Lecture Notes Day 3
+    
+    # Fibonacci 
+
+    cache = {}
+    def fib(n):
+        if n <= 1:
+            return n
+
+
+        # if n not in cache:
+            # cache[n] = fib(n-1) + fib(n-2)
+
+        # or just this
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = fib(n - 1) + fib(n - 2)
+        #    
+        
+        return cache[n]
+
+    print(fib(3))
+    print(fib(5))
+    print(fib(6))
+    print(fib(7))
+    print(fib(50))
+
+    # Cache 
+
+    import math
+
+    lookup_table = {}
+
+    def inverse_root(n):
+        return 1 / math.sqrt(n)
+
+    def build_lookup_table():
+        for i in range(1, 1000):
+            lookup_table[i] = inverse_root(i)
+    
+    build_lookup_table()
+
+    print(lookup_table[556])
+    print(lookup_table[99])
+    print(lookup_table[999])
+
+    # lazily = just in time, only when we need it
+
+    # dictionaries aren't in order because everything hashes differently
+
+    # Sorting
+
+    # lists keep things in order
+    # my_list = []
+    # my_list.append(1)
+    # my_list.append(2)
+    # my_list.append(3)
+    # my_list.append(4)
+    # my_list.append(5)
+    # ​
+    # # why are dictionaries not in order? i.e., order is not guaranteed
+    # ## everything hashes differently
+    # ## don't know what index the hash function will return
+    # ​
+    # my_list = [5, 3, 4, 2, 6, 7, 8, 1, 9]
+    # ​
+    # d = {
+    #     'Austin': 12,
+    #     'Michael': 24,
+    #     'Troy': 35,
+    #     'Jorge': 99,
+    #     'David': 42
+    # }
+    # ​
+    # # how can we sort our dictionary?
+    # ​
+    # # turn into a list
+    # for pair in d.items():
+    #     print(pair)
+    # ​
+    # # d.items().sort()
+    # ​
+    # print(sorted(d.items()))
+    # ​
+    # sorted_list_of_items = list(d.items())
+    # ​
+    # def anon_function(pair):
+    #     return pair[1]
+    # ​
+    # # sorted_list_of_items.sort(reverse=True, key=anon_function)
+    # sorted_list_of_items.sort(reverse=True, key=lambda pair: pair[1])
+    # ​
+    # print(sorted_list_of_items)
+    # ​
+    # # Erik's way with list comprehension: sorted([(v, k) for k, v in d.items()])
+    # ​
+    # # const my_func = x => x * 2
+    # ​
+    # something = list(map(lambda x, y:
+    #     x * 2, [1, 2, 3]))
+
+    # Print Letter Count
+
+    # # Understand
+    # # given a string
+    # ## return every letter and how many times it occurs in this string
+    # ## sorted by frequence of occurrence
+    # ​
+    # # Planning 
+    # # iterate through our string
+    # # tally the count using a dictionary
+    # # sort our dictionary by the value (i.e. the count of each letter)
+    # ​
+    # # upper- or lower-case everything
+    # # handle spaces
+    # ​
+    # # should we turn the string into a list first?
+    # ​
+    # # Execute
+    # def print_letter_count(s):
+    #     tally = {}
+    # ​
+    #     s = s.lower()
+    #     for character in s:
+    #         # if not char.isspace():
+    #         # if char != " ":
+    #         if character >= 'a' and character <= 'z':
+    #             if character not in tally:
+    #                 tally[character] = 1
+    #             else:
+    #                 tally[character] += 1
+    # ​
+    #     # print(sorted(tally.items(), key=lambda x: x[1], reverse=True))
+    #     # sorted gives back a new function
+    # ​
+    #     # sort works in place
+    #     tally_list = list(tally.items())
+    #     tally_list.sort(key=lambda x: x[1], reverse=True)
+    # ​
+    #     # alternate way to reverse sort
+    #     # tally_list.sort(key = lambda x: (-(x[1]))
+    # ​
+    #     for pair in tally_list:
+    #         print(f'Count: {pair[1]}, letter: {pair[0]}')
+    # ​
+    # ​
+    # ​
+    # print_letter_count("bunny hop")
+    # print_letter_count("The quick brown fox jumps over the lazy dog")
