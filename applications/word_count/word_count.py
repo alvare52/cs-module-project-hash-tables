7e89bd32c2a7ad92d78cddf1495d00b8f430afd2
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-07-09 19:34:04 -0700

    word count done

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -12,7 +12,15 @@ ignoredWords = {"\"": 0,
                  "/": 0,
                  "\\": 0,
                  "|": 0,
-                 "[": 0,}
+                 "[": 0,
+                 "]": 0,
+                 "{": 0,
+                 "}": 0,
+                 "(": 0,
+                 ")": 0,
+                 "*": 0,
+                 "^": 0,
+                 "&": 0}
 
 # takes in a string and returns a dict that has each word as a key and values are the times that words shows
 def word_count(s):
@@ -20,29 +28,39 @@ def word_count(s):
     # Your code here
     words = {}
     key = ""
+    i = 0
     # first try printing out every word 
-    for char in s:
-        if char == " ":
-            print("SPACE")
-        # end of word? add word to words with count and then reset key
-        if char.lower() in ignoredWords:
+    for char in s.lower():
+    
+        i += 1
+        # print(f"i = {i}")
+
+        # Space means end of word? Add word to words (with count) and then reset key
+        if char == " " or i == len(s):
+            if i == len(s) and char not in ignoredWords:
+                print(f"last, i = {i}, char is {char}")
+                key += char.lower()
+
             if key not in words:
                 words[key] = 1
             else:
                 words[key] += 1
-                print(f"key added = {key}")
-            
             key = ""
-        
-        # inside a word, add the next letter
+        # inside a word, add the next letter and not an ignored word
         else:
-            key += char.lower()
+            if char not in ignoredWords:
+                key += char.lower()
+
     return words
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
-    print(word_count("Hello there world"))
+    print(word_count("Hello the+re world"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
+txt = "Hello  there world!"
+x = txt.split()
+print(x)
